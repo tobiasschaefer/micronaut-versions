4edeb9a1d47a10e2705b2344047a7b74220d9ced
Tobias Schaefer <tobias.schaefer@novatec-gmbh.de>
2020-11-10 10:15:29 +0100

    mn 2.1.3 - added Kotlin

--- create.sh
+++ create.sh
@@ -1,2 +1,5 @@
 rm -rf micronaut-camunda-bpm-empty
 mn create-app micronaut-camunda-bpm-empty
+
+rm -rf micronaut-camunda-bpm-empty-kotlin
+mn create-app micronaut-camunda-bpm-empty-kotlin --lang=kotlin
