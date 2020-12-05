31ae312f7ef56fe9e7bc311301936517ea47c2c1
Tobias Schaefer <tobias.schaefer@novatec-gmbh.de>
2020-12-05 20:47:01 +0100

    switch from build=gradle_kotlin to gradle

--- create.sh
+++ create.sh
@@ -2,4 +2,4 @@ rm -rf micronaut-camunda-bpm-empty
 mn create-app micronaut-camunda-bpm-empty
 
 rm -rf micronaut-camunda-bpm-empty-kotlin
-mn create-app micronaut-camunda-bpm-empty-kotlin --lang=kotlin
+mn create-app micronaut-camunda-bpm-empty-kotlin --lang=kotlin --build=gradle
