7c9ce1187692daf2b334d21cc3a2dca2f8e546c9
Tobias Schaefer <tobias.schaefer@novatec-gmbh.de>
2021-01-23 07:49:58 +0100

    mn 2.3.0

--- micronaut-camunda-bpm-empty/src/test/java/micronaut/camunda/bpm/empty/MicronautCamundaBpmEmptyTest.java
+++ micronaut-camunda-bpm-empty/src/test/java/micronaut/camunda/bpm/empty/MicronautCamundaBpmEmptyTest.java
@@ -8,7 +8,7 @@ import org.junit.jupiter.api.Assertions;
 import javax.inject.Inject;
 
 @MicronautTest
-public class MicronautCamundaBpmEmptyTest {
+class MicronautCamundaBpmEmptyTest {
 
     @Inject
     EmbeddedApplication<?> application;
