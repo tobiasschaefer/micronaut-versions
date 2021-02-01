0177c9a2feec840f081976673b3606fc74048991
Tobias Schaefer <tobias.schaefer@novatec-gmbh.de>
2021-02-01 21:05:31 +0100

    Add java with maven

--- create.sh
+++ create.sh
@@ -1,3 +1,6 @@
+rm -rf micronaut-camunda-bpm-empty-java-mavene
+mn create-app micronaut-camunda-bpm-empty-java-maven --build=maven
+
 rm -rf micronaut-camunda-bpm-empty-java-gradle
 mn create-app micronaut-camunda-bpm-empty-java-gradle
 
